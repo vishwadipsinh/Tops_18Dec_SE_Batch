9aad651b9ce72c251e71ddde8934a9c3b84ab3d8
vishwadipsinh <vishwadipsinh-333@yahoo.com>
2024-06-27 19:35:30 +0530

    Digital Clock Temp

---
+++ Assessment/Digital Clock using JS/script.js
@@ -0,0 +1,27 @@
+
+setInterval(() => {
+    const time = document.querySelector(".display #time");
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let day_night = "AM";
+    if (hours > 12) {
+        day_night = "PM";
+        hours = hours - 12;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    time.textContent = hours + ":" + minutes + ":" + seconds + " " + day_night;
+
+    let days = [" Monday ", " Tuesday ", " Wednesday ", " Thursday ", " Friday ", " Saturday ", " Sunday ",];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",];
+});
